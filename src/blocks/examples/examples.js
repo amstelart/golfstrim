1bd225a1310780efa220617d340a3e04d1ed96ab
Артем <amstelart@MacBook-Pro-Artem.local>
2017-09-12 17:35:28 +0300

    Init

---
+++ src/blocks/examples/examples.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.examples-more-js').collapser({
+		mode: 'words',
+		truncate: 90,
+    showText: 'Подробнее',
+    hideText: 'Скрыть',
+    controlBtn: 'examples__more-link',
+    effect: 'fade'
+	});
+
+});
