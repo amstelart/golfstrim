c92aaeea80a3dcfbffc78b911f20b393bcad6687
amstelart <amsteldroid@gmail.com>
2018-04-06 04:13:30 +0300

    all

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
     items: 3,
     nav: true,
     margin:30,
-    navText: ['<svg width="26" height="48" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="26" height="48" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     loop: true
   });
 
@@ -52,7 +52,27 @@ $(document).ready(function(){
     items: 3,
     nav: false,
     margin:30,
-    loop: false
+    loop: false,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: false,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 2,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 3,
+      },
+      1200 : {
+        items: 3,
+      }
+    }
   });
 
   var demoNoUiSlider = document.getElementById('price-nouislider');
